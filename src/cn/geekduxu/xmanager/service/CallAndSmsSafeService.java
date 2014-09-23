2fbfb1277c23b8249026bf664a14711565c482bc
geekduxu <duxu0711@163.com>
2014-09-23 18:12:01 +0800

    09/23/2014.

--- src/cn/geekduxu/xmanager/service/CallAndSmsSafeService.java
+++ src/cn/geekduxu/xmanager/service/CallAndSmsSafeService.java
@@ -41,7 +41,9 @@ public class CallAndSmsSafeService extends Service {
 	
 	private InnerSmsReceiver smsReceiver;
 	private BlackListDao blackListDao;
-	
+	private TelephonyManager tm;
+	private MyListener listener;
+    
 	@Override
 	public IBinder onBind(Intent intent) {
 		return null;
@@ -54,12 +56,16 @@ public class CallAndSmsSafeService extends Service {
 		IntentFilter filter = new IntentFilter("android.provider.Telephony.SMS_RECEIVED");
 		filter.setPriority(IntentFilter.SYSTEM_HIGH_PRIORITY);
 		registerReceiver(smsReceiver, filter);
+        tm = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
+        listener = new MyListener();
+        tm.listen(listener, PhoneStateListener.LISTEN_CALL_STATE);
 	}
 	@Override
 	public void onDestroy() {
 		super.onDestroy();
 		unregisterReceiver(smsReceiver);
 		smsReceiver = null;
+        tm.listen(listener, PhoneStateListener.LISTEN_NONE);
 	}
 	private class InnerSmsReceiver extends BroadcastReceiver {
 		@Override
@@ -76,6 +82,67 @@ public class CallAndSmsSafeService extends Service {
 				}
 			}
 		}
+	}
+    
+    private class MyListener extends PhoneStateListener{
+		@Override
+		public void onCallStateChanged(int state, String incomingNumber) {
+			switch (state) {
+			case TelephonyManager.CALL_STATE_RINGING://响铃状态。
+				String result = blackListDao.exists(incomingNumber);
+				if("1".equals(result)||"3".equals(result)){
+					//观察呼叫记录数据库内容的变化。
+					Uri uri = Uri.parse("content://call_log/calls");
+					getContentResolver().registerContentObserver(uri, true, new CallLogObserver( incomingNumber,new Handler()));
+					endCall();
+				}
+				break;
+			}
+			super.onCallStateChanged(state, incomingNumber);
+		}
+	}
+
+	
+	private class CallLogObserver extends ContentObserver{
+		private String incomingNumber;
+
+		public CallLogObserver(String incomingNumber,Handler handler) {
+			super(handler);
+			this.incomingNumber = incomingNumber;
+		}
+
+		@Override
+		public void onChange(boolean selfChange) {
+			getContentResolver().unregisterContentObserver(this);
+			deleteCallLog(incomingNumber);
+			super.onChange(selfChange);
+		}
+		
+	}
+	
+	
+	public void endCall() {
+		//IBinder iBinder = ServiceManager.getService(TELEPHONY_SERVICE);
+		try {
+			//加载servicemanager的字节码
+			Class clazz = CallSmsSafeService.class.getClassLoader().loadClass("android.os.ServiceManager");
+			Method method = clazz.getDeclaredMethod("getService", String.class);
+			IBinder ibinder = (IBinder) method.invoke(null, TELEPHONY_SERVICE);
+			ITelephony.Stub.asInterface(ibinder).endCall();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * 利用内容提供者删除呼叫记录
+	 * @param incomingNumber
+	 */
+	public void deleteCallLog(String incomingNumber) {
+		ContentResolver  resolver = getContentResolver();
+		//呼叫记录uri的路径
+		Uri uri = Uri.parse("content://call_log/calls");
+		resolver.delete(uri, "number=?", new String[]{incomingNumber});
 	}
 
 }
